9a9e664547f9a759ac61e7cb24de5dc46ce627c7
Rayhan Bagus Sadewa <masbaguss001@gmail.com>
2025-12-30 04:37:51 +0700

    inherit input on flakes

--- flake.nix
+++ flake.nix
@@ -5,14 +5,19 @@
   };
 
   outputs = inputs: {
-
     nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+
+      specialArgs = {
+        inherit inputs;
+      };
+
       modules = [
-        { nix.settings.experimental-features = ["nix-command" "flakes"]; }
+        { nix.settings.experimental-features = [ "nix-command" "flakes" ]; }
         ./configuration.nix
       ];
     };
-
   };
+
   
 }
