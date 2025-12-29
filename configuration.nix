44e753ae11e9614080721f4df16ba894589b5f98
Rayhan Bagus Sadewa <masbaguss001@gmail.com>
2025-12-30 05:01:49 +0700

    add brave, integrating zsh as shell, remove gnome core apps

--- configuration.nix
+++ configuration.nix
@@ -58,14 +58,12 @@
   users.users.hann = {
     isNormalUser = true;
     description = "hann";
+    shell = pkgs.zsh;
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
     #  thunderbird
     ];
   };
-
-  # Install firefox.
-  programs.firefox.enable = true;
 
   programs.git = {
     enable = true;
@@ -81,48 +79,36 @@
   programs.vscode = {
     enable = true;
     defaultEditor = true;
-    package = pkgs.vscode;
     extensions = with pkgs.vscode-extensions; [ jnoortheen.nix-ide ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  programs.zsh = {
+    enable = true;
+    autosuggestions.enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+    shellAliases = {
+      btw = "echo This is NixOS, btw!";
+    };
+
+    histSize = 10000;
+    histFile = "$HOME/.zsh_history";
+    setOptions = [ "HIST_IGNORE_ALL_DUPS"];
+  };
+
+  services.gnome.core-apps.enable = false;
+  services.xserver.desktopManager.xterm.enable = false;
+  
+  nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     vim 
     wget
     nixd
+    brave
   ];
 
   nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
+  system.stateVersion = "25.11";
 }
